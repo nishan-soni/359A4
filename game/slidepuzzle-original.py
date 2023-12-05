0d3f818d16b92d9e37e1900e9e56b9c2ea8a24db
Nishan Soni <nishan.soni2@gmail.com>
2023-12-05 01:38:58 -0700

    clean up

--- game/slidepuzzle-original.py
+++ game/slidepuzzle-original.py
@@ -84,44 +84,6 @@ def main():
 
         checkForQuit()
 
-        # for event in pygame.event.get(): # event handling loop
-            # if event.type == MOUSEBUTTONUP:
-            #     spotx, spoty = getSpotClicked(mainBoard, event.pos[0], event.pos[1])
-
-            #     if (spotx, spoty) == (None, None):
-            #         # check if the user clicked on an option button
-            #         if RESET_RECT.collidepoint(event.pos):
-            #             resetAnimation(mainBoard, allMoves) # clicked on Reset button
-            #             allMoves = []
-            #         elif NEW_RECT.collidepoint(event.pos):
-            #             mainBoard, solutionSeq = generateNewPuzzle(80) # clicked on New Game button
-            #             allMoves = []
-            #         elif SOLVE_RECT.collidepoint(event.pos):
-            #             resetAnimation(mainBoard, solutionSeq + allMoves) # clicked on Solve button
-            #             allMoves = []
-            #     else:
-            #         # check if the clicked tile was next to the blank spot
-
-            #         blankx, blanky = getBlankPosition(mainBoard)
-            #         if spotx == blankx + 1 and spoty == blanky:
-            #             slideTo = LEFT
-            #         elif spotx == blankx - 1 and spoty == blanky:
-            #             slideTo = RIGHT
-            #         elif spotx == blankx and spoty == blanky + 1:
-            #             slideTo = UP
-            #         elif spotx == blankx and spoty == blanky - 1:
-            #             slideTo = DOWN
-
-            # elif event.type == KEYUP:
-            #     # check if the user pressed a key to slide a tile
-            #     if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
-            #         slideTo = LEFT
-            #     elif event.key in (K_RIGHT, K_d) and isValidMove(mainBoard, RIGHT):
-            #         slideTo = RIGHT
-            #     elif event.key in (K_UP, K_w) and isValidMove(mainBoard, UP):
-            #         slideTo = UP
-            #     elif event.key in (K_DOWN, K_s) and isValidMove(mainBoard, DOWN):
-            #         slideTo = DOWN
         direct = get_move()
         if direct != 'n':
             if direct == LEFT and isValidMove(mainBoard, LEFT):
